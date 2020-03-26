bce91ef40ab4d6e1d7121bcdc493fc969d9ce10a
Michele Rawlins <shellbop@att.net>
2020-03-25 22:53:29 -0500

    added stuff

---
+++ src/javascripts/components/home/home.js
@@ -0,0 +1,9 @@
+import utils from '../../helpers/utils';
+
+
+const myNavBar = () => {
+  const domString = '<h2> PINTEREST</h2>';
+  utils.printToDom('home', domString);
+};
+
+export default { myNavBar };
