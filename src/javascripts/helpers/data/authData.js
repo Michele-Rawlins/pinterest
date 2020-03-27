9d2f3b87c8e24f7742d76a529302d79583e1dda6
Michele Rawlins <shellbop@att.net>
2020-03-26 22:21:26 -0500

    added api example

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -5,6 +5,7 @@ import 'firebase/auth';
 const authDiv = $('#auth');
 const boardDiv = $('#boards');
 const pinDiv = $('#pins');
+const logoutButton = $('#navbar-logout-button');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
@@ -13,6 +14,7 @@ const checkLoginStatus = () => {
       authDiv.addClass('hide');
       boardDiv.removeClass('hide');
       pinDiv.removeClass('hide');
+      logoutButton.removeClass('hide');
     } else {
       // person is NOT logged in
       authDiv.removeClass('hide');
