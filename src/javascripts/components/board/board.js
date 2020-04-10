26cf7005cef1d409746a0340997d8f081845fa72
Michele Rawlins <shellbop@att.net>
2020-04-09 23:21:04 -0500

    boards print

--- src/javascripts/components/board/board.js
+++ src/javascripts/components/board/board.js
@@ -1,9 +1,11 @@
 const boardMaker = (board) => {
   let domString = '';
   domString += '<div class="col-3">';
-  domString += `<div class="card board-card" id=${board.name}>`;
-  domString += `<div class="card-header">${board.images}</div>`;
+  domString += `<div class="pin-card" id="${board.id}">`;
+  domString += `<div class="card-header">${board.name}</div>`;
+  domString += `<img class="card-img-top" src="${board.url}" alt="Board image for title of board">`;
   domString += '<div class="card-body">';
+  domString += '<button class="btn btn-danger delete-board"><i class="far fa-trash-alt"></i></button>';
   domString += '</div>';
   domString += '</div>';
   domString += '</div>';
