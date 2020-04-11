2aefeb91e9c7a3c47d877ea6818c624e276b38b1
Michele Rawlins <shellbop@att.net>
2020-04-11 18:13:40 -0500

    singleView works-wrongPlace

--- src/javascripts/components/board/board.js
+++ src/javascripts/components/board/board.js
@@ -1,7 +1,7 @@
 const boardMaker = (board) => {
   let domString = '';
   domString += '<div class="col-3">';
-  domString += `<div class="pin-card" id="${board.id}">`;
+  domString += `<div class="board-card" id="${board.id}">`;
   domString += `<div class="card-header">${board.name}</div>`;
   domString += `<img class="card-img-top" src="${board.url}" alt="Board image for title of board">`;
   domString += '<div class="card-body">';
--- src/javascripts/components/singleboard/singleboard.js
+++ src/javascripts/components/singleboard/singleboard.js
@@ -2,17 +2,19 @@ import smash from '../../helpers/data/smash';
 import utils from '../../helpers/utils';
 
 const buildBoard = (e) => {
-  const boardId = e.target.closest('.card').id;
-  smash.getSingleBoardrWithPins(boardId)
+  const boardId = e.target.closest('.board-card').id;
+  smash.getSingleBoardWithPins(boardId)
     .then((singleBoard) => {
+      console.error('singleboard', singleBoard);
       let domString = '';
-      domString += '<h2 class="text-center">Featured Farmer</h2>';
+      domString += '<h2 class="text-center">Featured Board</h2>';
       domString += '<div class="col-12">';
       domString += '<div class="card text-white bg-dark">';
-      domString += `<div class="card-header">Farmer ${singleBoard.name} (Age: ${singleBoard.age})</div>`;
+      domString += `<div class="card-header">Board ${singleBoard.name}</div>`;
       domString += '<div class="card-body">';
-      domString += '<h3 class="card-title">Cow(s) Owned:</h3>';
+      domString += '<h3 class="card-title">Pin(s) Owned:</h3>';
       singleBoard.pins.forEach((pin) => {
+        console.error('pin', pin);
         domString += `<p class="card-text">${pin.name} (${pin.url})</p>`;
       });
       domString += '</div>';
